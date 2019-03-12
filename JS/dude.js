53e9e558feb9eb46888e96ba81802feb47cb06db
S <rock59538@gmail.com>
2019-03-12 12:06:36 +0200

    i hope all BD

--- JS/dude.js
+++ JS/dude.js
@@ -2,7 +2,7 @@ var govno = document.getElementById('govno');
 
 var names = ['Igor', "Pisya", "Olya", "Tom"];
 
-function xhttr() {
+function BD() {
     var xhr = new XMLHttpRequest();
     var BD;
     xhr.open("GET", "../json/BD.json");
@@ -11,13 +11,13 @@ function xhttr() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             BD = JSON.parse(xhr.responseText);
             //console.log(BD);
-            govno.innerHTML += "<div class = 'qwe'>" + BD[0]['first_name'] + "</div>";
+            govno.innerHTML += "<div class = 'qwe'>" + BD[4]['img'] + "</div>";
         }
     });
     xhr.send();
 }
 
-xhttr();
+BD();
 
 //for (let i = 0; i < names.length; i++)
     //govno.innerHTML += "<div class = 'qwe'>" + names[i] + "</div>");
--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,80 @@
-function xhttr() {
+var mainMenShoes = document.getElementById('main-men-shoes');
+var mainMenSneakers = document.getElementById('main-men-sneakers');
+var mainMenBoots = document.getElementById('main-men-boots');
+
+function main_Men_Shoes(id, number, type) {
     var xhr = new XMLHttpRequest();
-    var BD;
-    xhr.open("GET", "../json/BD.json");
+    var bd;
+    xhr.open("GET", "../../json/BD.json");
 
     xhr.addEventListener("readystatechange", function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            BD = JSON.parse(xhr.responseText);
-            console.log(BD)
+            bd = JSON.parse(xhr.responseText);
+            for (var i = number; i < number + 4; i++){
+                if (bd[i]['type'] = type)
+                id.innerHTML +=
+                    "<a class='qwe'>" +
+                    "<div class='card'>" +
+                    "<img src=" + bd[i]['img-1'] +">" +
+                    "<hr>" +
+                    "<h2>" + bd[i]['firm'] + "</h2>" +
+                    "<h3>" + bd[i]['name'] + "</h3>" +
+                    "<h1>" + bd[i]['price'] + "</h1>" +
+                    "</div>" +
+                    "</a>";
+            }
         }
     });
     xhr.send();
 }
+main_Men_Shoes(mainMenShoes, 0, "shoes");
+main_Men_Shoes(mainMenSneakers, 4,"sneakers");
+main_Men_Shoes(mainMenBoots, 8, "boots");
 
-xhttr();
+/*
+var xhr = new XMLHttpRequest();
+var bd;
+xhr.open("GET", "../../json/BD.json");
 
+xhr.addEventListener("readystatechange", function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        bd = JSON.parse(xhr.responseText);
+        for (var i = 0; i < 4; i++){
+            id.innerHTML +=
+                "<a class='qwe'>" +
+                "<div class='card'>" +
+                "<img src=" + bd[i]['img'] +">" +
+                "<hr>" +
+                "<h2>" + bd[i]['firm'] + "</h2>" +
+                "<h3>" + bd[i]['name'] + "</h3>" +
+                "<h1>" + bd[i]['price'] + "</h1>" +
+                "</div>" +
+                "</a>";
+        }
+    }
+});
+xhr.send();
+}*/
+
+
+/* + bd[0]['img'] + */
+
+/*
+<a href="">
+  <div class="card">
+    <img src="../images/main-photo/men/Shoes/men-shoes-/men-shoes--.jpg" alt="">
+    <hr>
+    <h2>NIKE</h2>
+    <h3>sfsdfsdfsdfsdfsdfsdfsdf</h3>
+    <h1>149.99$</h1>
+  </div>
+</a>
+*/
+
+
+/*--------------------------------------------------------------------------------------------------------------------*/
 document.getElementById('slider-left').onclick = sliderLeft;
 document.getElementById('slider-right').onclick = sliderRight;
-document.getElementById('block-search').onclick = blockSearch;
 let stop = document.getElementById('slider-left');
 let stope = document.getElementById('slider-right');
 
@@ -48,17 +106,6 @@ function autoSlider() {
 stop.addEventListener('click', () => {
     clearInterval(timer);
 });
-
-
-
-
-function blockSearch() {
-    document.getElementById('block-search').style.paddingLeft = '50px';
-
-}
-
-
-
 
 
 
